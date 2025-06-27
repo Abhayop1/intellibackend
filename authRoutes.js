49f6b8f6aeb46d8414b90097a2b0c60f1f5e258f
Abhayop1 <abhayvageesha@gmail.com>
2025-06-27 16:20:29 +0530

    route

--- authRoutes.js
+++ authRoutes.js
@@ -1,9 +1,27 @@
+// const express = require('express');
+// const { signup, login } = require('./authController');
+
+// const router = express.Router();
+
+// router.post('/signup', signup);
+// router.post('/login', login);
+
+// module.exports = router;
+
+
+
 const express = require('express');
 const { signup, login } = require('./authController');
+const verifyToken = require('./auth'); // Import your verifyToken middleware
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
+
+router.post('/validate', verifyToken, (req, res) => {
+
+  res.json({ valid: true, message: 'Token is valid' });
+});
 
 module.exports = router;
